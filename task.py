accb4dcff695617ec49326c46e6b5c9ae7443e8d
EthanA-LCWC <144792227+EthanA-LCWC@users.noreply.github.com>
2024-09-17 12:52:47 +0000

    I made the code

--- task.py
+++ task.py
@@ -5,6 +5,11 @@ def task(): # Hint - you may need to create new, mid-point sets for the individu
     duplicate_fruits = None # This should be a tuple containing all the fruits in both tuples
     individual_fruits = None # This should be a tuple containing only the individual fruits
 
+    
+    duplicate_fruits = fruits_1.intersection(fruits_2)
+    individual_fruits = fruits_1.symmetric_difference(fruits_2)
+    individual_fruits.discard("sprite")
+    individual_fruits.discard("steak")
 
     return [duplicate_fruits, individual_fruits] # Note - functions can only return one data item - so both tuples
                                                  # are contained inside a single list
